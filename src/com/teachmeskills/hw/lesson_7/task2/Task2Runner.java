5a40c1d8d7b7c9b8a634328c457ff744c10bd95f
Marharyta Amosava <mozheliukrita@gmail.com>
2024-10-20 22:40:32 +0200

    Task2
    Task 2 refactoring:
     1) updated TaskRunner2 - added Total perimeters sum output

--- src/com/teachmeskills/hw/lesson_7/task2/Task2Runner.java
+++ src/com/teachmeskills/hw/lesson_7/task2/Task2Runner.java
@@ -17,16 +17,18 @@ public class Task2Runner {
         Figure[] figures = new Figure[5];
         figures[0] = new Triangle(3, 2, 3);
         figures[1] = new Rectangle(23, 54);
-        figures[2] = new Circle( 55);
-        figures[3] = new Rectangle( 12.4, 7.5);
-        figures[4] = new Triangle( 5, 6, 7);
+        figures[2] = new Circle(55);
+        figures[3] = new Rectangle(12.4, 7.5);
+        figures[4] = new Triangle(5, 6, 7);
 
         for (Figure figure : figures) {
             System.out.println("Figure is: " + figure.figureName);
             System.out.println("Area: " + Math.round(figure.calculateArea()));
             System.out.println("Perimeter: " + Math.round(figure.calculatePerimeter()) + "\n");
 
-            perimeterSum += figure.calculatePerimeter();
+            perimeterSum += Math.round(figure.calculatePerimeter());
         }
+
+        System.out.println("Total perimeters sum: " + Math.round(perimeterSum));
     }
 }
